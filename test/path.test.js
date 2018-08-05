bf87b90e6bb2290d473f71a0122caaa23ccfb047
kuu <maaail@hotmail.com>
2018-08-05 23:26:34 +0800

    Test cases

--- test/path.test.js
+++ test/path.test.js
@@ -1,12 +1,86 @@
-import { paramsFromPath } from '../src/path';
+import {
+    regexFromPath,
+    paramsFromPath,
+    joinPath,
+} from '../src/path';
 
-test('1', () => {
+test('path matching', () => {
+    expect(
+        regexFromPath('/order').test('/order')
+    ).toBeTruthy();
+    expect(
+        regexFromPath('/order').test('/order/')
+    ).toBeTruthy();
+    expect(
+        regexFromPath('/order').test('/order/1')
+    ).toBeFalsy();
+
+    expect(
+        regexFromPath('/order/').test('/order')
+    ).toBeTruthy();
+    expect(
+        regexFromPath('/order/').test('/order/')
+    ).toBeTruthy();
+    expect(
+        regexFromPath('/order/').test('/order/1')
+    ).toBeTruthy();
+});
+
+test('params', () => {
     expect(
         paramsFromPath(
-            '/order/:orderId',
-            '/order/112233abc',
+            '/order/:orderId/:itemId',
+            '/order/112233abc/222555',
         )
     ).toMatchObject({
         orderId: '112233abc',
+        itemId: '222555',
+    });
+
+    expect(
+        paramsFromPath(
+            '/order/:orderId/:orderId',
+            '/order/112233abc/222555',
+        )
+    ).toMatchObject({
+        orderId: '222555',
     });
 });
+
+test('join path', () => {
+    expect(
+        joinPath('/order', '/:orderId')
+    ).toEqual('/order/:orderId');
+    expect(
+        joinPath('order', ':orderId')
+    ).toEqual('/order/:orderId');
+    expect(
+        joinPath('/order', ':orderId')
+    ).toEqual('/order/:orderId');
+    expect(
+        joinPath('order/', ':orderId')
+    ).toEqual('/order/:orderId');
+    expect(
+        joinPath('/order/', ':orderId')
+    ).toEqual('/order/:orderId');
+    expect(
+        joinPath('/order', ':orderId')
+    ).toEqual('/order/:orderId');
+    expect(
+        joinPath('/order', ':orderId/')
+    ).toEqual('/order/:orderId/');
+    expect(
+        joinPath('/order', '/:orderId/')
+    ).toEqual('/order/:orderId/');
+
+    expect(
+        joinPath('/', '/order')
+    ).toEqual('/order');
+    expect(
+        joinPath(undefined, '/order')
+    ).toEqual('/order');
+    expect(
+        joinPath('', '/order')
+    ).toEqual('/order');
+});
+
